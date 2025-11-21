2e8fe71388960a351c43a8f21401e36a4b1cb94e
Trần Khánh Bình <tranbinh09122006@gmail.com>
2025-11-21 23:37:19 +0700

    fix DBConnection.java

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -4,15 +4,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
+    // Database connection details
     private static final String URL = "jdbc:mysql://localhost:3306/university_healthcare";
     private static final String USER = "root";
     private static final String PASSWORD = "123456";
+    
+    // driver is loaded only once
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Load MySQL JDBC Driver
+        } catch (ClassNotFoundException e) {
+            System.err.println("❌ JDBC Driver not found. Make sure mysql-connector-j.jar is added to lib folder!");
+            e.printStackTrace();
+        }
+    }
 
-    public static Connection getConnection() throws SQLException, ClassNotFoundException {
-        // Load driver
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        // return DB connection
+    // return DB connection
+    public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
-    
 }
