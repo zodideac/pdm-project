3b8f67455971af8b77f129a9c9ed0f039f98fab4
Trần Khánh Bình <tranbinh09122006@gmail.com>
2025-11-21 19:11:04 +0700

    update Staff.java

--- src/model/Staff.java
+++ src/model/Staff.java
@@ -1,5 +1,36 @@
 package model;
 
 public class Staff {
+    private int staffID;
+    private String staffName;
+    private String role;
+    private String address;
+
+    public Staff() {}
+
+    public Staff(int staffID, String staffName, String role, String address) {
+        this.staffID = staffID;
+        this.staffName = staffName;
+        this.role = role;
+        this.address = address;
+    }
+
+    // Getter & Setter
+    public int getStaffID() { return staffID; }
+    public void setStaffID(int staffID) { this.staffID = staffID; }
+
+    public String getStaffName() { return staffName; }
+    public void setStaffName(String staffName) { this.staffName = staffName; }
     
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    @Override
+    public String toString() {
+        return  "Staff [staffID=" + staffID + ", staffName=" + staffName + ", role=" + 
+        role + ", address=" + address + "]";
+    }
 }
